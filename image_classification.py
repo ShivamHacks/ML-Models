30959dbb1ce2027bd829b3195397ac5ad69471c5
Shivam Agrawal <shivamagrawal@wireless-10-104-33-22.umd.edu>
2019-04-14 00:01:11 -0400

    a

--- image_classification.py
+++ image_classification.py
@@ -2,6 +2,34 @@
 
 To download images:
 
-googleimagesdownload --keywords "Starbucks Cup" --limit 20 --output_directory /home/sagrawa2/ML/images/
+mkdir class_name
+googleimagesdownload --keywords "query" --limit 100 --output_directory /home/sagrawa2/ML/images/class_name --nodirectory
 
 """
+
+
+
+
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+
+base_model = InceptionV3(weights='imagenet', include_top=False)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(1, activation='sigmoid')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+	layer.trainable = False
+
+image_generator = image_datagen.flow_from_directory(
+	'images',
+	classes = None
+	class_mode = 'binary'
+)
+
+print(next(image_generator))
